13b34dc0d6cbd15ee7c648f9353fb7c6fa3df06a
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-05 21:11:17 +0200

    DontDestroyOnLoad settings

--- MainUnityProject/Assets/Scripts/SettingsManager.cs
+++ MainUnityProject/Assets/Scripts/SettingsManager.cs
@@ -14,6 +14,7 @@ public class SettingsManager : MonoBehaviour
         Instance = this;
         PermaLoad();
         settingsChangedEvent.AddListener(SaveSettings);
+        DontDestroyOnLoad(gameObject);
     }
 
     void OnDisable()
