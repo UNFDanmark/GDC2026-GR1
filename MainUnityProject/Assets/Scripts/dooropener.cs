78734804b76012abac92e49b2f8df11405eade7b
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-08 22:01:25 +0200

    Added more coroutines

---
+++ MainUnityProject/Assets/Scripts/dooropener.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class dooropener : MonoBehaviour
+{
+    bool open;
+    Animator animator;
+    public GameObject[] enemies;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open) return;
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null)
+                return;
+        }
+        open = true;
+        animator.SetTrigger("open");
+
+
+    }
+}
