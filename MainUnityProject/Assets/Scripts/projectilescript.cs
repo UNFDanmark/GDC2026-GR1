91f4f2f2f46f0ae2149ff8bbabfd9147a2bfc6cd
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-09 15:56:28 +0200

    Missile

--- MainUnityProject/Assets/Scripts/projectilescript.cs
+++ MainUnityProject/Assets/Scripts/projectilescript.cs
@@ -8,6 +8,7 @@ public class projectilescript : MonoBehaviour
     public float InitialSpeed;
     public float LifeTime;
     GameObject target;
+    public AudioSource shootAudioSource;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,7 @@ public class projectilescript : MonoBehaviour
         target = FindAnyObjectByType<PlayerController>().gameObject;
         rb.AddForce(new Vector3(0, InitialSpeed*rb.linearDamping, 0), ForceMode.Impulse);
         Destroy(gameObject, LifeTime);
+        shootAudioSource.Play();
     }
 
     // Update is called once per frame
