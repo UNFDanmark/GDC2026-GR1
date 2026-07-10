7d4dd8948d2df12802143a9f4067a7179fbab388
GDC2026 3D <gdc2026-3d@unf.dk>
2026-07-10 10:07:29 +0200

    awesum

---
+++ MainUnityProject/Assets/Scripts/SoundPlayerUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SoundPlayerUI: MonoBehaviour
+{
+    public AudioSource audioSourceEnter, audioSourceExit;
+    void Start()
+    {
+        SettingsManager.SettingsChangedEvent.AddListener(ReloadSettings);
+        ReloadSettings();
+    }
+
+    void OnDisable()
+    {
+        SettingsManager.SettingsChangedEvent.RemoveListener(ReloadSettings);
+    }
+
+    void ReloadSettings()
+    {
+        audioSourceEnter.volume = SettingsManager.SoundVolume;
+        audioSourceExit.volume = SettingsManager.SoundVolume;
+    }
+
+    void OnPointerEnter()
+    {
+        audioSourceEnter.Play();
+    }
+
+    void OnPointerExit()
+    {
+        audioSourceExit.Play();
+    }
+}
